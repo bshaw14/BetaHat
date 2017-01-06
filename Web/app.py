5f28064ec514993e3f5c336e803bd723ba95a1ce
Austin Graham <austingraham731@gmail.com>
2017-01-06 17:26:55 -0600

    API example started

--- Web/app.py
+++ Web/app.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask import request
+from random import randint
+import betahat
 
 app = Flask(__name__)
+models = dict()
 
-@app.route('/add', methods=['GET','POST'])
+@app.route('/add', methods=['GET'])
 def add():
-	a = request.args.get('a')
-	b = request.args.get('b')
-	return a + b
+	id = randint(1,100000)
+	if request.args.get('type') == 'nn':
+		models[id] = betahat.NeuralNetwork([2,3,1], 3, id)
+	return str(id)
+
+@app.route('/info', methods=['Get'])
+def info():
+	currentModel = models[int(request.args.get('id'))]
+	return str(currentModel.getSuccessRate())
 
 if __name__ == '__main__':
 	app.run(debug=True)
